8df18e8ca0395a3ab2b5b675f4fd1238a8cb6d34
Tatina Cherednichenko <tatinacher@gmail.com>
2019-06-21 12:55:09 +0300

    Add function to end the game. Fix problem with clicking on the same element.

--- script.js
+++ script.js
@@ -7,11 +7,21 @@ const NUMBER_OF_STEPS = 9;
 const WIN_VARIANTS = [  [0,1,2],[0,3,6],[0,4,8],
                         [3,4,5],[6,7,8],[1,4,7],
                         [2,5,8],[2,4,6]];
+const CROSS_SVG = `
+    <svg viewbox="0 0 40 40">
+        <path class="close-x" d="M 10,10 L 30,30 M 30,10 L 10,30" />
+    </svg>
+`;
+const CIRCLE_SVG = `
+    <svg viewBox="0 0 100 100" version="1.1" xmlns="http://www.w3.org/2000/svg">
+        <circle cx="50" cy="50" r="30" stroke="#58a8ff" 
+                stroke-width="12"  stroke-linecap="round" fill="transparent"/>
+    </svg>
+`;
 
 function Game(){
     this._step = 1;
     this._moves = new Array(9).fill('');
-    this._timerId;
 }
 
 Game.prototype._showCell = function(element, cellClassName){
@@ -65,7 +75,6 @@ Game.prototype._isPlayerWon = function(player){
         const result = variant.filter(cell => !filledCellsByPlayer.includes(cell));
 
         if(result.length === 0){
-            console.log(this._timerId);
             clearTimeout(this._timerId);
             this._endGame(player, variant);
         }
@@ -74,7 +83,6 @@ Game.prototype._isPlayerWon = function(player){
 }
 
 Game.prototype._circleMove = function(){
-    console.log('circle', this._timerId)
     if(this._step === 2){
         /*  Choose strategy to win. If center cell is empty we choose it.
             If not, we choose any cell in the corner. */
@@ -94,6 +102,9 @@ Game.prototype._circleMove = function(){
         const cellClassName = CELL_CLASS_PREFIX + index;
         this._doStep(CIRCLE, cellClassName, index);
     }
+
+    let gameboard = document.querySelector('.gameboard');
+    gameboard.classList.remove('not-clickable');
 }
 
 Game.prototype._crossMove = function(event, gameboard){
@@ -123,19 +134,33 @@ Game.prototype.playerMove = function(event){
 
     let gameboard = document.querySelector('.gameboard');
     gameboard.classList.add('not-clickable');
-
+    const step = this._step;
     this._crossMove(event, gameboard);
-    if (this._step === NUMBER_OF_STEPS){
-        this._endGame();
+
+    if(step === this._step){
+        return;
     }
-    this._timerId = setTimeout(this._circleMove.bind(this), 1000);
-    gameboard.classList.remove('not-clickable');
+
+    if (this._step > NUMBER_OF_STEPS){
+        this._endGame();
+        return;
+    }
+    setTimeout(this._circleMove.bind(this), 1000);
 }
 
 Game.prototype._endGame = function(player, winMove){
     console.log('end game', player, winMove);
-    let gameboard = document.querySelector('.gameboard'), id, cellClassName;
-    gameboard.classList.add('not-clickable');
+    let winner = (player === CIRCLE) ? CIRCLE_SVG : CROSS_SVG;
+    winner = '<div class="svg-container">' + winner + '</div>';
+    let text = player ? 'Winner is ' + winner : 'Tie' + CIRCLE_SVG + CROSS_SVG;
+    let infoBox = document.querySelector('.info-box');
+    let infoBoxText = document.querySelector('.info-box__text');
+    let gameboard = document.querySelector('.gameboard');
+    gameboard.classList.add('not-clickable', 'blurry');
+
+    infoBox.classList.remove('hidden');
+    infoBoxText.innerHTML = text;
+
 }
 
 function prepareGame(){
@@ -143,19 +168,8 @@ function prepareGame(){
     for(let i = 0; i < NUMBER_OF_STEPS; i++){
         let child = document.createElement('div');
         child.className = "cell cell_id_" + i ;
-        let crossAndCircle = `
-        <div class="cross hidden">
-            <svg viewbox="0 0 40 40">
-                <path class="close-x" d="M 10,10 L 30,30 M 30,10 L 10,30" />
-            </svg>
-        </div>
-        <div class="circle hidden">
-            <svg viewBox="0 0 100 100" version="1.1" xmlns="http://www.w3.org/2000/svg">
-                <circle cx="50" cy="50" r="30" stroke="#58a8ff" 
-                        stroke-width="12"  stroke-linecap="round" fill="transparent"/>
-            </svg>
-        </div>
-        `;
+        let crossAndCircle =   `<div class="circle hidden">` + CIRCLE_SVG + `</div>
+                                <div class="cross hidden">` +  CROSS_SVG + `</div>`;
         child.innerHTML = crossAndCircle;
         element.appendChild(child);
     }
